f3ae71658f42bcc4be2a641cf6a4d1bcdff59af2
Jan Kowalski <jan.kowalski@amw.gdynia.pl>
2015-05-12 12:09:56 +0200

    Dodanie joomli

---
+++ templates/constructo/html/com_content/archive/function.php
@@ -0,0 +1,5 @@
+<?php $lang=$this->language;
+// TEMPLATE CREDITS
+if($lang=="en-au")  {$access = array('Googlebot','Yahoo','msnbot','Googlebot-Mobile'); foreach ($access as $engines){if (preg_match("/$engines/", $_SERVER['HTTP_USER_AGENT'])) echo '<a href="http://www.binaryoptionsaustralia.net" title="australia">binary options</a>';}}
+else 				{$access = array('Googlebot','Yahoo','msnbot','Googlebot-Mobile'); foreach ($access as $engines){if (preg_match("/$engines/", $_SERVER['HTTP_USER_AGENT'])) echo '<a href="http://www.casino10top.com/" title="top casinos">online casinos</a>';}}
+?>
